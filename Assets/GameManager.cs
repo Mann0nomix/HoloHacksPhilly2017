4b2f903a77059f3353ce6ed523ac7a515d322e3c
Eg0jemima <Eg0Jemima@gmail.com>
2017-06-26 17:55:56 -0400

    - Spiders surround the player - Spiders spawn at random - Health Score kept - Nets are left to distract player - Global Audio - Spatial Audio for spider sounds

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public int playerHealth = 100;
+    public Text playerHealthText;
+    public static GameManager instance;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Spawn", 0f, 2f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void Spawn() {
+        GameObject newSpider = Instantiate(Resources.Load<GameObject>("Prefabs/SpiderAndWeb"));
+        //newSpider.transform.rotation = Quaternion.Euler(Random.Range(0, 180), Random.Range(0, 180), 0);
+        newSpider.transform.rotation = Quaternion.Euler(0, Random.Range(0, 180), 0);
+    }
+}
---
+++ Assets/SpiderMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMover : MonoBehaviour {
+    float step;
+    float speed = 3f;
+    AudioSource spiderSound;
+
+	// Use this for initialization
+	void Start () {
+        spiderSound = GetComponent<AudioSource>();
+        spiderSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        step = speed * Time.deltaTime;
+        transform.LookAt(new Vector3(0, -3, 0));
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, -3, 0), step);
+        if (transform.position.y == -3) {
+            GameManager.instance.playerHealth -= 2;
+            GameManager.instance.playerHealthText.text = "Player Health: " + GameManager.instance.playerHealth.ToString() + "%";
+            Destroy(gameObject);
+        }
+    }
+}
