bec2013702ec1c5a5cea03c5c2cdb5df288a1ada
Eg0jemima <Eg0Jemima@gmail.com>
2017-06-26 18:04:48 -0400

    - Added controllable spawn rate variable - Added max amount of spiderwebs default to 100

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,14 +7,17 @@ public class GameManager : MonoBehaviour {
     public int playerHealth = 100;
     public Text playerHealthText;
     public static GameManager instance;
+    float spawnRate = 0.5f;
+    GameObject spiders;
 
     private void Awake() {
         instance = this;
+        spiders = GameObject.Find("Spiders");
     }
 
     // Use this for initialization
     void Start () {
-        InvokeRepeating("Spawn", 0f, 2f);
+        InvokeRepeating("Spawn", 0f, spawnRate);
     }
 	
 	// Update is called once per frame
@@ -24,7 +27,11 @@ public class GameManager : MonoBehaviour {
 
     void Spawn() {
         GameObject newSpider = Instantiate(Resources.Load<GameObject>("Prefabs/SpiderAndWeb"));
+        newSpider.transform.parent = spiders.transform;
         //newSpider.transform.rotation = Quaternion.Euler(Random.Range(0, 180), Random.Range(0, 180), 0);
         newSpider.transform.rotation = Quaternion.Euler(0, Random.Range(0, 180), 0);
+        if(spiders.transform.childCount > 100) {
+            Destroy(spiders.transform.GetChild(0).gameObject);
+        }
     }
 }
